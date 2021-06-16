0d2dafc9e7e7a8ffb7497adc20cc1a55c2829fd3
Robert Conde <rconde2003@gmail.com>
2021-06-16 16:37:55 -0400

    I did a bad... NEVER TOUCH A BUTTON IF YOU DON'T KNOW WHAT IT DOES!!!!!
    ...

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-header("Location: https://" . $_SERVER['HTTP_HOST'] . "/user/login.php");
+header("Location: https://" . $_SERVER['HTTP_HOST'] . "/user/update.php");
--- require/checks.php
+++ require/checks.php
@@ -1,8 +1,26 @@
 <?php
 
-function checkPerms($permmin = 1) {
-	require_once $_SERVER['DOCUMENT_ROOT'] . "sql/standardSQL.php";
+function checkPerms($permmin = 1)
+{
+	require_once $_SERVER['DOCUMENT_ROOT'] . "require/sql.php";
 
-	if (getDetail('people', 'perms', $_SESSION['id']) <  $permmin)
-		die("You do not have the required permissions!");
+	if (!isset($_SESSION['id']))
+		die("<p style=\"color:red;\">You are not logged in!</p>\n");
+	elseif (getDetail('people', 'perms', $_SESSION['id']) < $permmin)
+		die("<p style=\"color:red;\">You do not have the required permissions!</p>\n");
+}
+
+function checkRankGreater($greater_id, $lower_id) {
+	require_once $_SERVER['DOCUMENT_ROOT'] . "require/sql.php";
+
+	$greater_perms = getDetail('people', 'perms', $greater_id);
+	$greater_rank = getRank($greater_id);
+
+	$lower_perms = getDetail('people', 'perms', $lower_id);
+	$lower_rank = getRank($lower_id);
+
+	if (is_null($lower_perms))
+		die("<p style=\"color:red;\">Account with ID = $lower_id does not exist!</p>\n");
+
+	return ($greater_rank > $lower_rank);
 }
