f8f51f84a56e801393603de21f398e3ff96992e4
Robert Conde <rconde2003@gmail.com>
2021-06-24 17:58:57 -0400

    Almost V1

---
+++ require/paymentManage.php
@@ -0,0 +1,51 @@
+<?php
+
+function isPaid($id, $payment_id) : bool
+{
+	echo "CHECKiNG ID :" . $payment_id;
+	require_once $_SERVER['DOCUMENT_ROOT'] . "require/sql.php";
+	$sql_conn = getDBConn();    // Get DB connection
+
+	// Check if already if already is paid
+	$find_payment_statement = $sql_conn->prepare("SELECT COUNT(*) FROM transactions WHERE id = ? AND payment_id = ?");
+
+	$find_payment_statement->bind_param('ss', $id, $payment_id);
+
+	if (!$find_payment_statement->execute())
+		die("Error occurred checking if payment was made: $find_payment_statement->error.");
+
+	$find_payment_statement->bind_result($num_rows);
+
+	$find_payment_statement->fetch();
+
+
+	echo "ROWS:$num_rows";
+	return ($num_rows > 0);
+}
+
+function togglePayment($id, $payment_id) : bool
+{
+	require_once $_SERVER['DOCUMENT_ROOT'] . "require/sql.php";
+	$sql_conn = getDBConn();    // Get DB connection
+
+	// If not already set as 'paid', then insert transaction (indicates 'paid'); else (currently indicating 'paid'), delete transaction
+	if (!isPaid($id, $payment_id)) {
+		echo "111";
+		$insert_transaction_statement = $sql_conn->prepare("INSERT INTO transactions(id, payment_id) VALUES (?, ?)");
+
+		$insert_transaction_statement->bind_param('ss', $id, $payment_id);
+
+		if (!$insert_transaction_statement->execute())
+			die("Error occurred inserting transaction: $insert_transaction_statement->error.");
+	} else {
+		echo "222";
+		$delete_transaction_statement = $sql_conn->prepare("DELETE FROM transactions WHERE id = ? AND payment_id = ?");
+
+		$delete_transaction_statement->bind_param('ss', $id, $payment_id);
+
+		if (!$delete_transaction_statement->execute())
+			die("Error occurred deleting transaction: $delete_transaction_statement->error.");
+	}
+
+	return true;
+}
--- user/update.php
+++ user/update.php
@@ -20,7 +20,7 @@ if (isset($_POST['update'])) {  // Process POST update
 //		echo "Key: $key; Value: $value<br>";
 //	}
 
-    if ($_SESSION['id'] != $_POST['id'] && !checkRankGreater($_SESSION['id'], $_POST['id']))   // Confirm rank is higher (so that people can't update through POST requests without being logged into an account of higher rank)
+    if ($_SESSION['id'] != $_POST['id'] && !checkCompareRank($_SESSION['id'], $_POST['id']))   // Confirm rank is higher (so that people can't update through POST requests without being logged into an account of higher rank)
 	    die("<p style=\"color:red;\">You do not have the required permissions!</p>\n");
 
 	require_once $_SERVER['DOCUMENT_ROOT'] . "require/basicAccountManage.php";
@@ -29,12 +29,12 @@ if (isset($_POST['update'])) {  // Process POST update
 }
 
 // View form (using correct ID)
-$update_id = $_SESSION['id'];
+$id = $_SESSION['id'];
 if (isset($_GET['id']) && $_GET['id'] != $_SESSION['id']) {
 	checkPerms(10);
 
-    if (checkRankGreater($_SESSION['id'], $_GET['id']))
-        $update_id = $_GET['id'];
+    if (checkCompareRank($_SESSION['id'], $_GET['id']))
+        $id = $_GET['id'];
     else
 	    die("<p style=\"color:red;\">You do not have the required permissions!</p>\n");
 }
@@ -48,7 +48,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "require/sql.php";
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "require/sql.php";
 if (getRank($_SESSION['id']) >= 1)
-    echo "<form method=\"get\" action=\"update.php\">\n",
+    echo "<form method=\"get\">\n",
          "<label for=\"id\"><i>Search ID:</i></label>\n",
          "<input id=\"id\" name=\"id\" type=\"search\" pattern=\"[0-9]{7}\" required>\n",
          "<input id=\"search\" type=\"submit\" value=\"View Account\">\n",
@@ -58,40 +58,40 @@ if (getRank($_SESSION['id']) >= 1)
 
 <h3>Account Information</h3>
 <?php
-if ($update_id != $_SESSION['id'])
+if ($id != $_SESSION['id'])
 	echo "<p style=\"color:violet;\"><i><b>Note:</b> You are updating an account that isn't yours, and has a permission rank below you!</i></p>\n";
 ?>
 
-<form method="post" action="update.php?id=<?php echo $update_id ?>">
-	<label for="id">ID
+<form method="post" action="update.php?id=<?php echo $id ?>">
+	<label for="id"><i>ID</i>
         <div class="tooltip"><i class="fa fa-question-circle"></i>
             <span class="tooltiptext"><i>Cannot</i> be updated!<br>Ask admin.</span>
         </div>:
     </label>
     <input id="id" name="id" type="text" pattern="[0-9]{7}" required
-           value="<?php echo $update_id ?>"
+           value="<?php echo $id ?>"
            readonly
            style="background: #ccc;"><br>
 	<br>
     <label for="fname">First Name:</label>
     <input id="fname" name="fname" type="text"
-           value="<?php echo getDetail('people', 'fname', $update_id) ?>"><br>
+           value="<?php echo getDetail('people', 'fname', $id) ?>"><br>
     <br>
     <label for="lname">Last Name:</label>
     <input id="lname" name="lname" type="text" required
-           value="<?php echo getDetail('people', 'lname', $update_id) ?>"><br>
+           value="<?php echo getDetail('people', 'lname', $id) ?>"><br>
     <br>
 	<label for="grade">Grade:</label>
 	<select id="grade" name="grade" required>
 		<option disabled></option>
-		<option value="6"  <?php echo getDetail('people', 'grade', $update_id) == 6 ? "selected" : "" ?>>6th Grade</option>
-		<option value="7"  <?php echo getDetail('people', 'grade', $update_id) == 7 ? "selected" : "" ?>>7th Grade</option>
-		<option value="8"  <?php echo getDetail('people', 'grade', $update_id) == 8 ? "selected" : "" ?>>8th Grade</option>
-		<option value="9"  <?php echo getDetail('people', 'grade', $update_id) == 9 ? "selected" : "" ?>>9th Grade</option>
-		<option value="10" <?php echo getDetail('people', 'grade', $update_id) == 10 ? "selected" : "" ?>>10th Grade</option>
-		<option value="11" <?php echo getDetail('people', 'grade', $update_id) == 11 ? "selected" : "" ?>>11th Grade</option>
-		<option value="12" <?php echo getDetail('people', 'grade', $update_id) == 12 ? "selected" : "" ?>>12th Grade</option>
-		<option value="0"  <?php echo getDetail('people', 'grade', $update_id) == 0 ? "selected" : "" ?>>Not a Student</option>
+		<option value="6"  <?php echo getDetail('people', 'grade', $id) == 6 ? "selected" : "" ?>>6th Grade</option>
+		<option value="7"  <?php echo getDetail('people', 'grade', $id) == 7 ? "selected" : "" ?>>7th Grade</option>
+		<option value="8"  <?php echo getDetail('people', 'grade', $id) == 8 ? "selected" : "" ?>>8th Grade</option>
+		<option value="9"  <?php echo getDetail('people', 'grade', $id) == 9 ? "selected" : "" ?>>9th Grade</option>
+		<option value="10" <?php echo getDetail('people', 'grade', $id) == 10 ? "selected" : "" ?>>10th Grade</option>
+		<option value="11" <?php echo getDetail('people', 'grade', $id) == 11 ? "selected" : "" ?>>11th Grade</option>
+		<option value="12" <?php echo getDetail('people', 'grade', $id) == 12 ? "selected" : "" ?>>12th Grade</option>
+		<option value="0"  <?php echo getDetail('people', 'grade', $id) == 0 ? "selected" : "" ?>>Not a Student</option>
 	</select><br>
 	<br>
 	<label for="email">Email
@@ -100,7 +100,7 @@ if ($update_id != $_SESSION['id'])
 		</div>:
 	</label>
 	<input id="email" name="email" type="email" required
-           value="<?php echo getDetail('people', 'email', $update_id) ?>"><br>
+           value="<?php echo getDetail('people', 'email', $id) ?>"><br>
 	<br>
 	<label for="phone">Phone Number
 		<div class="tooltip"><i class="fa fa-question-circle"></i>
@@ -108,7 +108,7 @@ if ($update_id != $_SESSION['id'])
 		</div>:
 	</label>
 	<input id="phone" name="phone" type="tel" pattern="[0-9]{10}" required
-           value="<?php echo getDetail('people', 'phone', $update_id) ?>"><br>
+           value="<?php echo getDetail('people', 'phone', $id) ?>"><br>
 	<br>
 	<label for="division">Division
 		<div class="tooltip"><i class="fa fa-question-circle"></i>
@@ -117,18 +117,18 @@ if ($update_id != $_SESSION['id'])
 	</label>
 	<select id="division" name="division" required>
 		<option disabled></option>
-		<option value="1" <?php echo getDetail('people', 'division', $update_id) == 1 ? "selected" : "" ?>>Algebra I</option>
-		<option value="2" <?php echo getDetail('people', 'division', $update_id) == 2 ? "selected" : "" ?>>Geometry</option>
-		<option value="3" <?php echo getDetail('people', 'division', $update_id) == 3 ? "selected" : "" ?>>Algebra II</option>
-		<option value="4" <?php echo getDetail('people', 'division', $update_id) == 4 ? "selected" : "" ?>>Precalculus</option>
-		<option value="5" <?php echo getDetail('people', 'division', $update_id) == 5 ? "selected" : "" ?>>Calculus</option>
-		<option value="6" <?php echo getDetail('people', 'division', $update_id) == 6 ? "selected" : "" ?>>Statistics</option>
-		<option value="0" <?php echo getDetail('people', 'division', $update_id) == 0 ? "selected" : "" ?>>Not a Student</option>
+		<option value="1" <?php echo getDetail('people', 'division', $id) == 1 ? "selected" : "" ?>>Algebra I</option>
+		<option value="2" <?php echo getDetail('people', 'division', $id) == 2 ? "selected" : "" ?>>Geometry</option>
+		<option value="3" <?php echo getDetail('people', 'division', $id) == 3 ? "selected" : "" ?>>Algebra II</option>
+		<option value="4" <?php echo getDetail('people', 'division', $id) == 4 ? "selected" : "" ?>>Precalculus</option>
+		<option value="5" <?php echo getDetail('people', 'division', $id) == 5 ? "selected" : "" ?>>Calculus</option>
+		<option value="6" <?php echo getDetail('people', 'division', $id) == 6 ? "selected" : "" ?>>Statistics</option>
+		<option value="0" <?php echo getDetail('people', 'division', $id) == 0 ? "selected" : "" ?>>Not a Student</option>
 	</select><br>
 	<br>
 	<input id="update" name="update" type="submit"
            value="Update<?php
-           if ($update_id != $_SESSION['id'])
+           if ($id != $_SESSION['id'])
                echo " (Someone Else's Account)";
            ?>">
 </form>
