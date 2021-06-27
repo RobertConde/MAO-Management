af1b8faf17f7a717f82dca3b16517c66857332ca
Robert Conde <rconde2003@gmail.com>
2021-06-27 15:33:06 -0400

    V1 Complete (I think)
    * Made fieldset `inline-block`
    * Changed 'View Account' button to 'Search'
    * Surrounded update and register form with `fieldset`
    * Added legends to register and login
    * Upon login, will be __directly__ be redirected to index page
    * `.readonly` background colour changed

--- user/register.php
+++ user/register.php
@@ -7,57 +7,61 @@ navigationBar();
 <html>
 <h2><u>MAO Account Registration</u></h2>
 <form method="post" action="register.php">
-	<label for="id">ID:</label>
-	<input id= "id" name="id" type="text" pattern="[0-9]{7}" required><br>
-	<br>
-    <label for="fname">First Name:</label>
-    <input id="fname" name="fname" type="text" required><br>
-    <br>
-    <label for="lname">Last Name:</label>
-    <input id="lname" name="lname" type="text" required><br>
-    <br>
-	<label for="grade">Grade:</label>
-	<select id="grade" name="grade" required>
-        <option disabled selected></option>
-		<option value="6">6th Grade</option>
-		<option value="7">7th Grade</option>
-		<option value="8">8th Grade</option>
-		<option value="9">9th Grade</option>
-		<option value="10">10th Grade</option>
-		<option value="11">11th Grade</option>
-		<option value="12">12th Grade</option>
-		<option value="0">Not a Student</option>
-	</select><br>
-	<br>
-	<label for="email">Email
-        <div class="tooltip"><i class="fa fa-question-circle"></i>
-            <span class="tooltiptext">Will be used to send you <i>login codes</i> for your account!</span>
-        </div>:
-    </label>
-	<input id="email" name="email" type="email" required><br>
-	<br>
-	<label for="phone">Phone Number
-        <div class="tooltip"><i class="fa fa-question-circle"></i>
-            <span class="tooltiptext">Must be a 10-digit US phone number. Only type the digits!</span>
-        </div>:
-    </label>
-	<input id="phone" name="phone" type="tel" pattern="[0-9]{10}" required><br>
-	<br>
-	<label for="division">Division
-        <div class="tooltip"><i class="fa fa-question-circle"></i>
-            <span class="tooltiptext">Select the division you <i>will</i> compete the most this competition cycle.</span>
-        </div>:
-    </label>
-	<select id="division" name="division" required>
-        <option disabled selected></option>
-		<option value="1">Algebra I</option>
-		<option value="2">Geometry</option>
-		<option value="3">Algebra II</option>
-		<option value="4">Precalculus</option>
-		<option value="5">Calculus</option>
-		<option value="6">Statistics</option>
-		<option value="0">Not a Student</option>
-	</select><br>
+    <fieldset>
+        <legend><b>Account Information</b></legend>
+
+        <label for="id">ID:</label>
+        <input id= "id" name="id" type="text" pattern="[0-9]{7}" required><br>
+        <br>
+        <label for="fname">First Name:</label>
+        <input id="fname" name="fname" type="text" required><br>
+        <br>
+        <label for="lname">Last Name:</label>
+        <input id="lname" name="lname" type="text" required><br>
+        <br>
+        <label for="grade">Grade:</label>
+        <select id="grade" name="grade" required>
+            <option disabled selected></option>
+            <option value="6">6th Grade</option>
+            <option value="7">7th Grade</option>
+            <option value="8">8th Grade</option>
+            <option value="9">9th Grade</option>
+            <option value="10">10th Grade</option>
+            <option value="11">11th Grade</option>
+            <option value="12">12th Grade</option>
+            <option value="0">Not a Student</option>
+        </select><br>
+        <br>
+        <label for="email">Email
+            <div class="tooltip"><i class="fa fa-question-circle"></i>
+                <span class="tooltiptext">Will be used to send you <i>login codes</i> for your account!</span>
+            </div>:
+        </label>
+        <input id="email" name="email" type="email" required><br>
+        <br>
+        <label for="phone">Phone Number
+            <div class="tooltip"><i class="fa fa-question-circle"></i>
+                <span class="tooltiptext">Must be a 10-digit US phone number. Only type the digits!</span>
+            </div>:
+        </label>
+        <input id="phone" name="phone" type="tel" pattern="[0-9]{10}" required><br>
+        <br>
+        <label for="division">Division
+            <div class="tooltip"><i class="fa fa-question-circle"></i>
+                <span class="tooltiptext">Select the division you <i>will</i> compete the most this competition cycle.</span>
+            </div>:
+        </label>
+        <select id="division" name="division" required>
+            <option disabled selected></option>
+            <option value="1">Algebra I</option>
+            <option value="2">Geometry</option>
+            <option value="3">Algebra II</option>
+            <option value="4">Precalculus</option>
+            <option value="5">Calculus</option>
+            <option value="6">Statistics</option>
+            <option value="0">Not a Student</option>
+        </select>
+    </fieldset><br>
 	<br>
     <input id="register" name="register" type="submit" value="Register">
 </form>
--- user/update.php
+++ user/update.php
@@ -51,7 +51,7 @@ if (getRank($_SESSION['id']) >= 1)
     echo "<form method=\"get\">\n",
          "<label for=\"id\"><i>Search ID:</i></label>\n",
          "<input id=\"id\" name=\"id\" type=\"search\" pattern=\"[0-9]{7}\" required>\n",
-         "<input id=\"search\" type=\"submit\" value=\"View Account\">\n",
+         "<input id=\"search\" type=\"submit\" value=\"Search\">\n",
          "</form>\n",
          "<hr>\n";
 ?>
@@ -63,74 +63,77 @@ if ($id != $_SESSION['id'])
 ?>
 
 <form method="post" action="update.php?id=<?php echo $id ?>">
-	<label for="id"><i>ID</i>
-        <div class="tooltip"><i class="fa fa-question-circle"></i>
-            <span class="tooltiptext"><i>Cannot</i> be updated!<br>Ask admin.</span>
-        </div>:
-    </label>
-    <input id="id" name="id" type="text" pattern="[0-9]{7}" required
-           value="<?php echo $id ?>"
-           readonly
-           style="background: #ccc;"><br>
-	<br>
-    <label for="fname">First Name:</label>
-    <input id="fname" name="fname" type="text"
-           value="<?php echo getDetail('people', 'fname', $id) ?>"><br>
+    <fieldset>
+        <label for="id"><i>ID</i>
+            <div class="tooltip"><i class="fa fa-question-circle"></i>
+                <span class="tooltiptext"><i>Cannot</i> be updated!<br>Ask admin.</span>
+            </div>:
+        </label>
+        <input id="id" name="id" type="text" pattern="[0-9]{7}" required
+               value="<?php echo $id ?>"
+               readonly
+               style="background: #ccc;"><br>
+        <br>
+        <label for="fname">First Name:</label>
+        <input id="fname" name="fname" type="text"
+               value="<?php echo getDetail('people', 'fname', $id) ?>"><br>
+        <br>
+        <label for="lname">Last Name:</label>
+        <input id="lname" name="lname" type="text" required
+               value="<?php echo getDetail('people', 'lname', $id) ?>"><br>
+        <br>
+        <label for="grade">Grade:</label>
+        <select id="grade" name="grade" required>
+            <option disabled></option>
+            <option value="6"  <?php echo getDetail('people', 'grade', $id) == 6 ? "selected" : "" ?>>6th Grade</option>
+            <option value="7"  <?php echo getDetail('people', 'grade', $id) == 7 ? "selected" : "" ?>>7th Grade</option>
+            <option value="8"  <?php echo getDetail('people', 'grade', $id) == 8 ? "selected" : "" ?>>8th Grade</option>
+            <option value="9"  <?php echo getDetail('people', 'grade', $id) == 9 ? "selected" : "" ?>>9th Grade</option>
+            <option value="10" <?php echo getDetail('people', 'grade', $id) == 10 ? "selected" : "" ?>>10th Grade</option>
+            <option value="11" <?php echo getDetail('people', 'grade', $id) == 11 ? "selected" : "" ?>>11th Grade</option>
+            <option value="12" <?php echo getDetail('people', 'grade', $id) == 12 ? "selected" : "" ?>>12th Grade</option>
+            <option value="0"  <?php echo getDetail('people', 'grade', $id) == 0 ? "selected" : "" ?>>Not a Student</option>
+        </select><br>
+        <br>
+        <label for="email">Email
+            <div class="tooltip"><i class="fa fa-question-circle"></i>
+                <span class="tooltiptext">Will be used to send you <i>login codes</i> for your account!</span>
+            </div>:
+        </label>
+        <input id="email" name="email" type="email" required
+               value="<?php echo getDetail('people', 'email', $id) ?>"><br>
+        <br>
+        <label for="phone">Phone Number
+            <div class="tooltip"><i class="fa fa-question-circle"></i>
+                <span class="tooltiptext">Must be a 10-digit US phone number. Only type the digits!</span>
+            </div>:
+        </label>
+        <input id="phone" name="phone" type="tel" pattern="[0-9]{10}" required
+               value="<?php echo getDetail('people', 'phone', $id) ?>"><br>
+        <br>
+        <label for="division">Division
+            <div class="tooltip"><i class="fa fa-question-circle"></i>
+                <span class="tooltiptext">Select the division you <i>will</i> compete the most this competition cycle.</span>
+            </div>:
+        </label>
+        <select id="division" name="division" required>
+            <option disabled></option>
+            <option value="1" <?php echo getDetail('people', 'division', $id) == 1 ? "selected" : "" ?>>Algebra I</option>
+            <option value="2" <?php echo getDetail('people', 'division', $id) == 2 ? "selected" : "" ?>>Geometry</option>
+            <option value="3" <?php echo getDetail('people', 'division', $id) == 3 ? "selected" : "" ?>>Algebra II</option>
+            <option value="4" <?php echo getDetail('people', 'division', $id) == 4 ? "selected" : "" ?>>Precalculus</option>
+            <option value="5" <?php echo getDetail('people', 'division', $id) == 5 ? "selected" : "" ?>>Calculus</option>
+            <option value="6" <?php echo getDetail('people', 'division', $id) == 6 ? "selected" : "" ?>>Statistics</option>
+            <option value="0" <?php echo getDetail('people', 'division', $id) == 0 ? "selected" : "" ?>>Not a Student</option>
+        </select>
+    </fieldset><br>
     <br>
-    <label for="lname">Last Name:</label>
-    <input id="lname" name="lname" type="text" required
-           value="<?php echo getDetail('people', 'lname', $id) ?>"><br>
-    <br>
-	<label for="grade">Grade:</label>
-	<select id="grade" name="grade" required>
-		<option disabled></option>
-		<option value="6"  <?php echo getDetail('people', 'grade', $id) == 6 ? "selected" : "" ?>>6th Grade</option>
-		<option value="7"  <?php echo getDetail('people', 'grade', $id) == 7 ? "selected" : "" ?>>7th Grade</option>
-		<option value="8"  <?php echo getDetail('people', 'grade', $id) == 8 ? "selected" : "" ?>>8th Grade</option>
-		<option value="9"  <?php echo getDetail('people', 'grade', $id) == 9 ? "selected" : "" ?>>9th Grade</option>
-		<option value="10" <?php echo getDetail('people', 'grade', $id) == 10 ? "selected" : "" ?>>10th Grade</option>
-		<option value="11" <?php echo getDetail('people', 'grade', $id) == 11 ? "selected" : "" ?>>11th Grade</option>
-		<option value="12" <?php echo getDetail('people', 'grade', $id) == 12 ? "selected" : "" ?>>12th Grade</option>
-		<option value="0"  <?php echo getDetail('people', 'grade', $id) == 0 ? "selected" : "" ?>>Not a Student</option>
-	</select><br>
-	<br>
-	<label for="email">Email
-		<div class="tooltip"><i class="fa fa-question-circle"></i>
-			<span class="tooltiptext">Will be used to send you <i>login codes</i> for your account!</span>
-		</div>:
-	</label>
-	<input id="email" name="email" type="email" required
-           value="<?php echo getDetail('people', 'email', $id) ?>"><br>
-	<br>
-	<label for="phone">Phone Number
-		<div class="tooltip"><i class="fa fa-question-circle"></i>
-			<span class="tooltiptext">Must be a 10-digit US phone number. Only type the digits!</span>
-		</div>:
-	</label>
-	<input id="phone" name="phone" type="tel" pattern="[0-9]{10}" required
-           value="<?php echo getDetail('people', 'phone', $id) ?>"><br>
-	<br>
-	<label for="division">Division
-		<div class="tooltip"><i class="fa fa-question-circle"></i>
-			<span class="tooltiptext">Select the division you <i>will</i> compete the most this competition cycle.</span>
-		</div>:
-	</label>
-	<select id="division" name="division" required>
-		<option disabled></option>
-		<option value="1" <?php echo getDetail('people', 'division', $id) == 1 ? "selected" : "" ?>>Algebra I</option>
-		<option value="2" <?php echo getDetail('people', 'division', $id) == 2 ? "selected" : "" ?>>Geometry</option>
-		<option value="3" <?php echo getDetail('people', 'division', $id) == 3 ? "selected" : "" ?>>Algebra II</option>
-		<option value="4" <?php echo getDetail('people', 'division', $id) == 4 ? "selected" : "" ?>>Precalculus</option>
-		<option value="5" <?php echo getDetail('people', 'division', $id) == 5 ? "selected" : "" ?>>Calculus</option>
-		<option value="6" <?php echo getDetail('people', 'division', $id) == 6 ? "selected" : "" ?>>Statistics</option>
-		<option value="0" <?php echo getDetail('people', 'division', $id) == 0 ? "selected" : "" ?>>Not a Student</option>
-	</select><br>
-	<br>
-	<input id="update" name="update" type="submit"
+    <input id="update" name="update" type="submit"
            value="Update<?php
            if ($id != $_SESSION['id'])
                echo " (Someone Else's Account)";
            ?>">
+
 </form>
 </html>
 
