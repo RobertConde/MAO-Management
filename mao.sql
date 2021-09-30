f9dd471413f8427fb819c47cd4568af0dfcbc6c4
Robert Conde <rconde2003@gmail.com>
2021-09-30 18:13:45 -0400

    Remove Sensitive Info and Fixed `mao.sql`
    * Removed sensitive info from `config.ini`
    * Create table if necessary called `mao` and use that to create tables

--- mao.sql
+++ mao.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- Database: `mao`
 --
 
+CREATE DATABASE IF NOT EXISTS `mao` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `mao`;
+
 -- --------------------------------------------------------
 
 --
