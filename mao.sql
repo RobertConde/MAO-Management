ca09acce2c72ad5d7724a74a2f134b0cf3ed650b
Robert Conde <rconde2003@gmail.com>
2021-10-14 07:54:03 -0400

    Updated `.htaccess` for compatibility and `mao.sql` for import in DB instead of creating DB

--- mao.sql
+++ mao.sql
@@ -17,11 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `mao`
---
-CREATE DATABASE IF NOT EXISTS `mao` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `mao`;
 
 -- --------------------------------------------------------
 
